3b2ef5e5c88b07193c40b58378900bc840e73f7b
makogra <pietrzak.maciek.pv@gmail.com>
2024-02-23 18:34:49 +0100

    feat init

---
+++ src/main/java/com/pietrzak/maciek/gitrepoapilookup/GitRepoApiLookupApplication.java
@@ -0,0 +1,13 @@
+package com.pietrzak.maciek.gitrepoapilookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepoApiLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepoApiLookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pietrzak/maciek/gitrepoapilookup/GitRepoApiLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pietrzak.maciek.gitrepoapilookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRepoApiLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
