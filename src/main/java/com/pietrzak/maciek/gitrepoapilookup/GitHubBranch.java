da894e9c8dab773d7bc3721aa8f6ce7ace0d596d
makogra <pietrzak.maciek.pv@gmail.com>
2024-03-12 18:32:05 +0100

    feat use of java 21 records

--- src/main/java/com/pietrzak/maciek/gitrepoapilookup/GitHubBranch.java
+++ src/main/java/com/pietrzak/maciek/gitrepoapilookup/GitHubBranch.java
@@ -4,33 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
 
-public class GitHubBranch {
-
-    String name;
-    String sha;
-
-
-    public GitHubBranch() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSha() {
-        return sha;
-    }
-
-    public void setSha(String sha) {
-        this.sha = sha;
-    }
-
+public record GitHubBranch(
+        String name,
+        String sha
+) {
     @JsonProperty("commit")
-    public void unpackSha(Map<String, String> commit) {
-        this.sha = commit.get("sha");
+    public GitHubBranch unpackSha(Map<String, String> commit) {
+        return new GitHubBranch(this.name(), commit.get("sha"));
     }
 }
--- src/main/java/com/pietrzak/maciek/gitrepoapilookup/Repo.java
+++ src/main/java/com/pietrzak/maciek/gitrepoapilookup/Repo.java
@@ -4,41 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
 
-public class Repo {
-
-    private String name;
-    private String login;
-    private  boolean fork;
-
-    public Repo() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public boolean isFork() {
-        return fork;
-    }
-
-    public void setFork(boolean fork) {
-        this.fork = fork;
-    }
-
+public record Repo(
+        String name,
+        String login,
+        boolean fork
+) {
     @JsonProperty("owner")
-    public void unpackLogin(Map<String, String> owner) {
-        this.login = owner.get("login");
+    public Repo unpackLogin(Map<String, String> owner) {
+        return new Repo(this.name(), owner.get("login"), this.fork());
     }
 }
